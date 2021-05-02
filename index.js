ba2a65a8453eba5757d01e5e01d9880e29614027
tomer stoler <tomersto1@gmail.com>
2021-05-03 00:56:58 +0300

    almostThere

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const missionDetails = document.querySelector('#')
-
-
 const containerDiv = document.createElement('div')
-containerDiv.style.height = "400px"
+containerDiv.className = "containerDiv"
 document.body.appendChild(containerDiv)
 
-const missionDetails = {
-    details : inputDetails,
-    date : missionDate,
-    hour : missionHour,
+const detailsInput = document.querySelector('#detailsInput')
+const dateInput = document.querySelector('#dateInput')
+const hourInput = document.querySelector('#hourInput')
+
+
+const main = () => {
+
 }
 
 const form = document.querySelector('#form')
 form.addEventListener('submit', (e) => {
-e.preventDefault();
-
-const task = document.createElement('div')
-task.style.backgroundImage = "url('notebg.png')";
-task.style.scale = ""
-const taskContent = document.createElement('div')
-taskContent.style.padding = "7%"
-task.appendChild(taskContent)
-task.style.display = "inline-block"
-task.style.height = "55%"
-task.style.width = "10%"
-task.style.margin = "2%"
-task.id = Date.now();
-
+    e.preventDefault();
+    
+    const task = document.createElement('div')
+    task.className = "task"
+   
+    // const taskContent = document.createElement('div')
+    // taskContent.style.padding = "7%"
+    // task.appendChild(taskContent)
+    task.style.height = "59%"
+    task.style.width = "10%"
+    task.style.margin = "2%"
+    task.style.padding = "0.8%"
+    task.id = Date.now();
+    
+    const missionDetails = {
+        // 'details' : inputDetails,
+        details : form.details.value,
+        date : form.date.value,
+        hour : form.hour.value,
+    
+    }
 
 const deleteButtonDiv = document.createElement('div')
 deleteButtonDiv.innerHTML = `
@@ -34,13 +41,28 @@ deleteButtonDiv.innerHTML = `
 task.appendChild(deleteButtonDiv)
 
 const taskTextDiv = document.createElement('div')
-taskTextDiv.innerHTML = ""
+taskTextDiv.innerHTML = missionDetails.details;
+taskTextDiv.style.flex = "1"
+taskTextDiv.style.overflowY = "auto"
+taskTextDiv.style.flexDirection = 'column'
+taskTextDiv.style.flexWrap = "nowrap"
+console.log(missionDetails.details)
+task.appendChild(taskTextDiv)
 
-const taskButton = document.createElement('button');
-taskButton.type = "submit"
+const taskDateDiv = document.createElement('div')
+if(!missionDetails.date || !missionDetails.hour || !missionDetails.details ) {
+    alert('missing parameters')
+    return
+}
+else {
+
+    taskDateDiv.innerHTML = missionDetails.date + ' / ' + missionDetails.hour;
+}
+taskDateDiv.className = "taskDateDiv"
+task.appendChild(taskDateDiv)
 
 // task.style.marginTop = "20%"
-task.style.padding = "0.8%"
+
 containerDiv.appendChild(task)
 
 

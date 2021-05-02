6e49e30403216ba1b8cbf79dfdb36650ced401ae
tomer stoler <tomersto1@gmail.com>
2021-05-03 01:11:33 +0300

    edited-code

--- index.js
+++ index.js
@@ -1,70 +1,53 @@
+const formEl = document.querySelector('#form')
+
 const containerDiv = document.createElement('div')
 containerDiv.className = "containerDiv"
 document.body.appendChild(containerDiv)
 
-const detailsInput = document.querySelector('#detailsInput')
-const dateInput = document.querySelector('#dateInput')
-const hourInput = document.querySelector('#hourInput')
-
-
-const main = () => {
-
-}
-
-const form = document.querySelector('#form')
-form.addEventListener('submit', (e) => {
+formEl.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+    const missionDetails = {
+        details: formEl.details.value,
+        date: formEl.date.value,
+        hour: formEl.hour.value,
+    }
+
+    createTask(missionDetails)
+})
+
+const createTask = (missionDetails) => {
     const task = document.createElement('div')
     task.className = "task"
-   
-    // const taskContent = document.createElement('div')
-    // taskContent.style.padding = "7%"
-    // task.appendChild(taskContent)
     task.style.height = "59%"
     task.style.width = "10%"
     task.style.margin = "2%"
     task.style.padding = "0.8%"
     task.id = Date.now();
-    
-    const missionDetails = {
-        // 'details' : inputDetails,
-        details : form.details.value,
-        date : form.date.value,
-        hour : form.hour.value,
-    
+
+    const deleteButtonDiv = document.createElement('div')
+    deleteButtonDiv.innerHTML = `
+    <button class="btn btn-primary">press me</button>`
+    task.appendChild(deleteButtonDiv)
+
+    const taskTextDiv = document.createElement('div')
+    taskTextDiv.innerHTML = missionDetails.details;
+    taskTextDiv.style.flex = "1"
+    taskTextDiv.style.overflowY = "auto"
+    taskTextDiv.style.flexDirection = 'column'
+    taskTextDiv.style.flexWrap = "nowrap"
+    task.appendChild(taskTextDiv)
+
+    const taskDateDiv = document.createElement('div')
+    taskDateDiv.className = "taskDateDiv"
+    if (!missionDetails.date || !missionDetails.hour || !missionDetails.details) {
+        alert('missing parameters')
+        return
     }
+    else {
 
-const deleteButtonDiv = document.createElement('div')
-deleteButtonDiv.innerHTML = `
-<button class="btn btn-primary">press me</button>`
-task.appendChild(deleteButtonDiv)
+        taskDateDiv.innerHTML = missionDetails.date + ' / ' + missionDetails.hour;
+    }
+    task.appendChild(taskDateDiv)
 
-const taskTextDiv = document.createElement('div')
-taskTextDiv.innerHTML = missionDetails.details;
-taskTextDiv.style.flex = "1"
-taskTextDiv.style.overflowY = "auto"
-taskTextDiv.style.flexDirection = 'column'
-taskTextDiv.style.flexWrap = "nowrap"
-console.log(missionDetails.details)
-task.appendChild(taskTextDiv)
-
-const taskDateDiv = document.createElement('div')
-if(!missionDetails.date || !missionDetails.hour || !missionDetails.details ) {
-    alert('missing parameters')
-    return
+    containerDiv.appendChild(task)
 }
-else {
-
-    taskDateDiv.innerHTML = missionDetails.date + ' / ' + missionDetails.hour;
-}
-taskDateDiv.className = "taskDateDiv"
-task.appendChild(taskDateDiv)
-
-// task.style.marginTop = "20%"
-
-containerDiv.appendChild(task)
-
-
-})
-

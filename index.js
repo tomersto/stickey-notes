df11c7e81870e7597bd2712bf9b395851f665230
tomer stoler <tomersto1@gmail.com>
2021-05-07 21:57:40 +0300

    functional_LocalStorage

--- index.js
+++ index.js
@@ -1,48 +1,115 @@
-const missionDetails = document.querySelector('#')
+const formEl = document.querySelector("#form");
+const hourInput = document.querySelector('#hourInput')
+const dateInput = document.querySelector('#dateInput')
+const detailsInput = document.querySelector('#detailsInput')
+const containerDiv = document.createElement("div");
+containerDiv.className = "containerDiv";
+containerDiv.style.display = "flex";
+document.body.appendChild(containerDiv);
+
+const storageValue = 'notes'
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const missionDetails = {
+    details: formEl.details.value,
+    date: formEl.date.value,
+    hour: formEl.hour.value,
+    id: Date.now(),
+  };
+  if (!missionDetails.date || !missionDetails.hour || !missionDetails.details) {
+    alert("missing parameters bruh");
+    return;
+  }
+  saveToLocalStorage(missionDetails)
+  loadFromLocalStorage()
+
+  hourInput.value = ""
+  dateInput.value = ""
+  detailsInput.value = ""
+});
 
 
-const containerDiv = document.createElement('div')
-containerDiv.style.height = "400px"
-document.body.appendChild(containerDiv)
+const createTask = (missionDetails) => {
+  console.log(missionDetails)
+  const task = document.createElement("div");
+  task.className = "task";
+  task.style.height = "59%";
+  task.style.width = "10%";
+  task.style.margin = "2%";
+  task.style.padding = "0.8%";
+  task.id = missionDetails.id;
 
-const missionDetails = {
-    details : inputDetails,
-    date : missionDate,
-    hour : missionHour,
+  const deleteButtonDiv = document.createElement("div");
+  deleteButtonDiv.className = "deleteButtonDiv"
+
+  task.appendChild(deleteButtonDiv);
+
+  const taskTextDiv = document.createElement("div");
+  taskTextDiv.innerHTML = missionDetails.details;
+  taskTextDiv.style.flex = "1";
+  taskTextDiv.style.overflow = "hidden auto";
+  taskTextDiv.style.maxWidth = "100%";
+  task.appendChild(taskTextDiv);
+
+  const taskDateDiv = document.createElement("div");
+  taskDateDiv.className = "taskDateDiv";
+  taskDateDiv.innerHTML = missionDetails.date + " / " + missionDetails.hour;
+  task.appendChild(taskDateDiv);
+
+  const deleteButton = document.createElement('button')
+  deleteButton.className = "glyphicon glyphicon-remove"
+  deleteButton.id = "deleteButton"
+
+  deleteButton.addEventListener('click', () => removeTask(missionDetails.id))
+  deleteButtonDiv.appendChild(deleteButton)
+
+  return task;
+
+};
+
+function loadFromLocalStorage() {
+  containerDiv.innerHTML = ""
+  const data = JSON.parse(localStorage.getItem(storageValue)) || [];
+  console.log(data)
+  data.forEach(task => {
+    containerDiv.appendChild(createTask(task))
+  })
+}
+function saveToLocalStorage(data) {
+  let exisitingData = JSON.parse(getLocalStorageItem(storageValue))
+  if (exisitingData) {
+    exisitingData.push(data)
+  }
+  else {
+    exisitingData = [data]
+  }
+
+  localStorage.setItem(storageValue, JSON.stringify(exisitingData))
 }
 
-const form = document.querySelector('#form')
-form.addEventListener('submit', (e) => {
-e.preventDefault();
+function removeTask(id) {
+  console.log(id)
+  const taskToDelete = document.getElementById(id)
+  removeFromLocalStorage(id)
+  taskToDelete.remove()
+}
+loadFromLocalStorage()
 
-const task = document.createElement('div')
-task.style.backgroundImage = "url('notebg.png')";
-task.style.scale = ""
-const taskContent = document.createElement('div')
-taskContent.style.padding = "7%"
-task.appendChild(taskContent)
-task.style.display = "inline-block"
-task.style.height = "55%"
-task.style.width = "10%"
-task.style.margin = "2%"
-task.id = Date.now();
+function removeFromLocalStorage(id) {
+  const existingData = JSON.parse(getLocalStorageItem(storageValue)) || [];
+  for (let i = 0; i < existingData.length; i++) {
+    if (existingData[i].id == id)
+      existingData.splice(i, 1)
+  }
+  // existingData.forEach(task => {
+  //   if(task.id == id){
+  //     existingData.splice(index, 1)
+  //   }
+  // })
+  localStorage.setItem(storageValue, JSON.stringify(existingData))
+}
 
-
-const deleteButtonDiv = document.createElement('div')
-deleteButtonDiv.innerHTML = `
-<button class="btn btn-primary">press me</button>`
-task.appendChild(deleteButtonDiv)
-
-const taskTextDiv = document.createElement('div')
-taskTextDiv.innerHTML = ""
-
-const taskButton = document.createElement('button');
-taskButton.type = "submit"
-
-// task.style.marginTop = "20%"
-task.style.padding = "0.8%"
-containerDiv.appendChild(task)
-
-
-})
-
+function getLocalStorageItem(item) {
+  return localStorage.getItem(item)
+}
